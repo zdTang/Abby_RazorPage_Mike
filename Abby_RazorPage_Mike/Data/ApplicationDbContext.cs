a6ed17f1d70a3d01029fbb9c2d72ef15e01d3400
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-06-19 09:58:34 -0400

    2-4 Add ApplicationDbContext

---
+++ Abby_RazorPage_Mike/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Abby_RazorPage_Mike.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abby_RazorPage_Mike.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+    }
+}
