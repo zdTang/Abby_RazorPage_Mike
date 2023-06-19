6b23c2bd6b5fbe4b18b2b8fd9f1e1e11f0df0472
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-06-19 09:52:11 -0400

    3-1 Add Category model

---
+++ Abby_RazorPage_Mike/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Abby_RazorPage_Mike.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
