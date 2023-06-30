5945d0b901227120eddd79940db4ae3ae507375e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-06-30 10:56:28 -0400

    3-5 Create property in Page Model
    Understand how to add a property to Model and use it in Razor Pages

--- Abby_RazorPage_Mike/Pages/Categories/Create.cshtml.cs
+++ Abby_RazorPage_Mike/Pages/Categories/Create.cshtml.cs
@@ -1,9 +1,11 @@
+using Abby_RazorPage_Mike.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Abby_RazorPage_Mike.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category Category { get; set; }
         public void OnGet()
         {
 
