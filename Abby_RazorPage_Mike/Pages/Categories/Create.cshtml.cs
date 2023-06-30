b2a7cb6ef52ee25354b30d9dcb2684c8494528c8
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-06-30 10:39:07 -0400

    3-4 Create Category UI

---
+++ Abby_RazorPage_Mike/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby_RazorPage_Mike.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
