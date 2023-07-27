d4b46ea09c89ef2286e724730266808cd12b3a56
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-27 16:05:37 -0400

    3-10 Display Name and Range Annotation

--- Abby_RazorPage_Mike/Model/Category.cs
+++ Abby_RazorPage_Mike/Model/Category.cs
@@ -8,6 +8,8 @@ namespace Abby_RazorPage_Mike.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be in range of 1-100!!!")]
         public int DisplayOrder { get; set; }
     }
 }
