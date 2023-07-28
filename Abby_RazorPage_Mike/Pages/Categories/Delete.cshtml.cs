134be931265d5369d6a831d013117a43dabb95a7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-28 16:33:29 -0400

    3-15 Delete Category - my approach
    can delete Category ,but cannot go back to Category/index

---
+++ Abby_RazorPage_Mike/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,24 @@
+using Abby_RazorPage_Mike.Data;
+using Abby_RazorPage_Mike.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby_RazorPage_Mike.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category? Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task OnGetAsync(int id)
+        {
+            Category = _db.Category.Find(id);
+            _db.Category.Remove(Category);
+            await _db.SaveChangesAsync();
+            RedirectToPage("../Categories/Index");
+        }
+    }
+}
