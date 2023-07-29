2606b7b0862bc12c38e5110399d27e26f1569211
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-29 06:35:48 -0400

    3-15 Delete Category - Tutor's Approach

--- Abby_RazorPage_Mike/Pages/Categories/Delete.cshtml.cs
+++ Abby_RazorPage_Mike/Pages/Categories/Delete.cshtml.cs
@@ -5,21 +5,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Abby_RazorPage_Mike.Pages.Categories
 {
+    [BindProperties]
     public class DeleteModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        public Category? Category { get; set; }
+
+        public Category Category { get; set; }
 
         public DeleteModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public async Task<IActionResult> OnGetAsync(int id)
+        public void OnGet(int id)
         {
             Category = _db.Category.Find(id);
-            _db.Category.Remove(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index");
+            //Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+            //Category = _db.Category.SingleOrDefault(u=>u.Id==id);
+            //Category = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var categoryFromDb = _db.Category.Find(Category.Id);
+            if (categoryFromDb != null)
+            {
+                _db.Category.Remove(categoryFromDb);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+
+            }
+
+            return Page();
         }
     }
+
 }
