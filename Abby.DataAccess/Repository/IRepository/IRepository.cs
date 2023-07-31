097f5d86ead226dff2eb251291267364adca0b20
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-31 08:57:58 -0400

    5-1 Create IRepository

---
+++ Abby.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //GET ALL, GET By ID FIRST OR DEFAULT, ADD, REMOVE, REMOVERANGE
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        IEnumerable<T> GetAll();
+        // This use lambda expression, can get anything based on given logic
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
