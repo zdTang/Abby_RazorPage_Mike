7d4cfab87167a456bc2efe8fa7c4305e04ac57e6
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-31 14:38:00 -0400

    5-2 Implement Repository

---
+++ Abby.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext? _dbContext;
+        internal DbSet<T> dbSet;  //Internal types or members are accessible only within files in the same assembly
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            this.dbSet = _dbContext.Set<T>();   // Get this T type Entity from dbContext
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            //IQueryable<T> query = dbSet;
+            //return query.ToList();
+            return dbSet.AsQueryable<T>().ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
