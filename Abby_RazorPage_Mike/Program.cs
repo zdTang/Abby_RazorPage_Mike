7805b0f56b694b72c52c72a343f757707bf37373
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-07-31 15:21:30 -0400

    5-4  Register Category Repository to Container

--- Abby_RazorPage_Mike/Program.cs
+++ Abby_RazorPage_Mike/Program.cs
@@ -1,4 +1,6 @@
 using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository;
+using Abby.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 namespace Abby_RazorPage_Mike
@@ -14,6 +16,9 @@ namespace Abby_RazorPage_Mike
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+            // The following are for DI registeration
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
