6f15c11463ba32a8e5b9c6c5eb2fce02ce8c1b4f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-01 18:44:29 -0400

    6-3-1 Create MenuItem Repository

---
+++ Abby.DataAccess/Repository/IRepository/IMenuItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using Abby.Models;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IMenuItemRepository : IRepository<MenuItem>
+    {
+        void Update(MenuItem menuItem);
+        //void Save();   // Save should not be in the Repository level
+    }
+}
---
+++ Abby.DataAccess/Repository/MenuItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+
+namespace Abby.DataAccess.Repository
+{
+    public class MenuItemRepository : Repository<MenuItem>, IMenuItemRepository
+    {
+        private readonly ApplicationDbContext _db;
+        // Need to pass dbContext to Parent Class
+        public MenuItemRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(MenuItem menuItem)
+        {
+            var objFromDb = _db.MenuItem.FirstOrDefault(u => u.Id == menuItem.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = menuItem.Name;
+                objFromDb.Description = menuItem.Description;
+                objFromDb.Price = menuItem.Price;
+                objFromDb.CategoryId = menuItem.CategoryId;
+                objFromDb.FoodTypeId = menuItem.FoodTypeId;
+                if (objFromDb.Image != null) // Here is important! Sometimes user only update other perperties                                  
+                {                            // In order to don't delete existed image URL,We need check here.
+                    objFromDb.Image = menuItem.Image;
+                }
+            }
+        }
+    }
+}
