607ee52d942d701b67f91934c8ebff6064ba20cf
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-01 18:59:49 -0400

    6-3-2 Add MenuItem to UnitOfWork

--- Abby.DataAccess/Repository/UnitOfWork.cs
+++ Abby.DataAccess/Repository/UnitOfWork.cs
@@ -7,11 +7,13 @@ namespace Abby.DataAccess.Repository
         private readonly ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
         public IFoodTypeRepository FoodType { get; private set; }
+        public IMenuItemRepository MenuItem { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             FoodType = new FoodTypeRepository(_db);     // instantiate an Repository
+            MenuItem = new MenuItemRepository(_db);
         }
 
 
