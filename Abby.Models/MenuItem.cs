fbfec847bd308db31c7ecb122eaa0b8583ab8b27
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-01 17:48:43 -0400

    6-1 Create MenuItem model

---
+++ Abby.Models/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Abby.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [Range(1, 1000, ErrorMessage = "Price should be between $1 and $1000")]
+        public double Price { get; set; }
+        public int FoodTypeId { get; set; }
+        [ForeignKey("FoodTypeId")]  // Use this Attribute to denote this is a Foreign Key
+        public FoodType FoodType { get; set; } // Add a Navigation property, Or the Foreign Key will not be recognized
+        public int CategoryId { get; set; }//As this propertyName is Category+Id, the entityFramework will regard it as Foreign key
+        public Category Category { get; set; } // Add a Navigation property, Or the Foreign Key will not be recognized
+    }
+}
