8d2d66fd7d512b2f4abd493af1b5fba8a87c9c78
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-04 12:39:14 -0400

    6-6 Menu Item Upsert Page Get Hander UI

--- Abby.Models/MenuItem.cs
+++ Abby.Models/MenuItem.cs
@@ -13,9 +13,11 @@ namespace Abby.Models
         public string Image { get; set; }
         [Range(1, 1000, ErrorMessage = "Price should be between $1 and $1000")]
         public double Price { get; set; }
+        [Display(Name = "Food Type")]            // The FoodType dropdown list will map to this property "FoodTypeId", but we don't want "FoodTypeId" as a Lable
         public int FoodTypeId { get; set; }
-        [ForeignKey("FoodTypeId")]  // Use this Attribute to denote this is a Foreign Key
+        [ForeignKey("FoodTypeId")]             // Use this Attribute to denote this is a Foreign Key
         public FoodType FoodType { get; set; } // Add a Navigation property, Or the Foreign Key will not be recognized
+        [Display(Name = "Category")]           // The Category dropdown list will map to this property "CategoryId",  but we don't want "CategoryId" as a Lable
         public int CategoryId { get; set; }//As this propertyName is Category+Id, the entityFramework will regard it as Foreign key
         public Category Category { get; set; } // Add a Navigation property, Or the Foreign Key will not be recognized
     }
--- Abby_RazorPage_Mike/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ Abby_RazorPage_Mike/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Abby.DataAccess.Repository.IRepository;
 using Abby.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Abby_RazorPage_Mike.Pages.Admin.MenuItems
 {
@@ -16,20 +17,35 @@ namespace Abby_RazorPage_Mike.Pages.Admin.MenuItems
         // and we cannot check its Id at all.
         // for an new object, all its properties are null, but as the Id is integer,
         // it will be parse as 0.
-        public MenuItem menuItem { get; set; } = new MenuItem();
+        public MenuItem MenuItem { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }    // Used for UI to draw dropdown list 
+        public IEnumerable<SelectListItem> FoodTypeList { get; set; }    // Used for UI to draw dropdown list
         private readonly IUnitOfWork _unitOfWork;                        // UnitOfWork pattern
         public UpsertModel(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            MenuItem = new MenuItem();
         }
         public void OnGet()
         {
+            // Using projecting -- Select
+            CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+            FoodTypeList = _unitOfWork.FoodType.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
         }
         public IActionResult OnPost()
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.MenuItem.Add(menuItem!);    // UnitOfWork pattern
+                _unitOfWork.MenuItem.Add(MenuItem);    // UnitOfWork pattern
                 _unitOfWork.Save();                    // UnitOfWork pattern
                 TempData["success"] = "MenuItem created successfully";
                 return RedirectToPage("Index");
