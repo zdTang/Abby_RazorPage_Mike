b0c06462eba083e396242bc761cfa68489e79924
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-05 22:20:05 -0400

    6-9-1 Create API  -- Tang's approach
    use WebAPI framework  other than MVC

---
+++ Abby_RazorPage_Mike/Controllers/MenuItemTwoController.cs
@@ -0,0 +1,23 @@
+﻿using Abby.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abby_RazorPage_Mike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemTwoController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        //public IEnumerable<MenuItem>? MenuItem { get; set; } // we will not use it as model any more, so comment it
+        public MenuItemTwoController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var menuItemList = _unitOfWork.MenuItem.GetAll();
+            return Ok(new { data = menuItemList });
+        }
+    }
+}
