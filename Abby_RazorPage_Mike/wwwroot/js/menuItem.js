c346770e0e140dc3a918f82e5ad7cb3bd79dc376
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-07 10:15:46 -0400

    6-13 DataTable render two buttons

--- Abby_RazorPage_Mike/wwwroot/js/menuItem.js
+++ Abby_RazorPage_Mike/wwwroot/js/menuItem.js
@@ -10,6 +10,18 @@
             { "data": "price", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" >
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-success text-white mx-2">
+                            <i class="fa-solid fa-pencil"></i>  </a>
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-danger text-white mx-2">
+                             <i class="fa-solid fa-trash"></i>  </a>
+                            </div>`
+                },
+                "width": "15%"
+            }
         ],
         "width": "100%"
     });
