2a32e4e00d41f3d16a26ca5ae0d5964df8d3a0fd
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-08 14:58:20 -0400

    6-16 Implement Delete with REST API

--- Abby_RazorPage_Mike/Controllers/MenuItemController.cs
+++ Abby_RazorPage_Mike/Controllers/MenuItemController.cs
@@ -9,16 +9,32 @@ namespace Abby_RazorPage_Mike.Controllers
     public class MenuItemController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
         //public IEnumerable<MenuItem>? MenuItem { get; set; } // we will not use it as model any more, so comment it
-        public MenuItemController(IUnitOfWork unitOfWork)
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
         [HttpGet]
         public IActionResult Index()
         {
             var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
             return Json(new { data = menuItemList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _unitOfWork.MenuItem.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful." });
         }
     }
 }
