b3d0432ea033d0b80b56e43caf071223d0561d2d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-11 10:00:46 -0400

    8-3 Extend Identity User

--- Abby.DataAccess/Data/AbbyDbContext.cs
+++ Abby.DataAccess/Data/AbbyDbContext.cs
@@ -14,5 +14,6 @@ namespace Abby.DataAccess.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<FoodType> FoodType { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; } // This table derived IdentityUser class
     }
 }
