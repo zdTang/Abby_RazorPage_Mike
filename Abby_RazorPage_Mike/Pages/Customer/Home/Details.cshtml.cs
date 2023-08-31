a19fdefcf6a784536fa4fdcfca116adab1eee343
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-31 11:20:15 -0400

    9-3 Add Shopping Cart on Detailed Page

--- Abby_RazorPage_Mike/Pages/Customer/Home/Details.cshtml.cs
+++ Abby_RazorPage_Mike/Pages/Customer/Home/Details.cshtml.cs
@@ -14,13 +14,15 @@ namespace Abby_RazorPage_Mike.Pages.Customer.Home
             _unitOfWork = unitOfWork;
         }
 
-        public MenuItem MenuItem { get; set; }
-        [Range(1, 100, ErrorMessage = "Please select a count between 1 and 100")]
-        public int Count { get; set; }
+        [BindProperty]
+        public ShoppingCart ShoppingCart { get; set; }
 
         public void OnGet(int id)
         {
-            MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,FoodType");
+            ShoppingCart = new()
+            {
+                MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,FoodType")
+            };
         }
     }
 }
