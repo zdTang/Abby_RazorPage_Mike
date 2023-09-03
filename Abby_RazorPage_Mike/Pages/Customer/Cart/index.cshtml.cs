b68e4b3ae3b665826a0bea0ba97c6f574b0c7eed
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-03 11:06:58 -0400

    9-7 Shopping Cart Get Handler

---
+++ Abby_RazorPage_Mike/Pages/Customer/Cart/index.cshtml.cs
@@ -0,0 +1,28 @@
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Abby_RazorPage_Mike.Pages.Customer.Cart
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        private readonly IUnitOfWork _unitOfWork;
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(filter: u => u.ApplicationUserId == claim.Value);
+            }
+        }
+    }
+}
