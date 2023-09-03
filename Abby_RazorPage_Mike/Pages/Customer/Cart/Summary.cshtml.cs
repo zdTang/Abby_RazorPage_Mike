85ea34bba464154328aa8aaa5ad45c3658495157
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-03 17:21:09 -0400

    9-13 Cart Summary UI

---
+++ Abby_RazorPage_Mike/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby_RazorPage_Mike.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
